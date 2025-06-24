9ea01eaa2a0b61038c381089a732f61d52bf31bc
91828 <richiedilipkurian@gmail.com>
2025-06-24 16:48:28 +0100

    set up basic structure of contest app and edit few

--- contest/__init__.py
+++ contest/__init__.py
@@ -9,25 +9,64 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'contest'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 2
+    ENDOWMENT = Currency(10)
+    COST_PER_TICKET = Currency(0.50)
+    PRIZE = Currency(10)
 
 
 class Subsession(BaseSubsession):
-    pass
+    is_paid = models.BooleanField()
 
+    def setup_round(self):
+        self.is_paid = True
+        for group in self.get_groups():
+            group.setup_round()
+
+    def compute_outcome(self):
+        for group in self.get_groups():
+            group.compute_outcome()
 
 class Group(BaseGroup):
-    pass
+    prize = models.CurrencyField()
 
+    def setup_round(self):
+        self.prize = C.PRIZE
+        for player in self.get_players():
+            player.setup_round()
+
+    def compute_outcome(self):
+        total = sum (player.tickets_purchased for player in self.get_players() )
 
 class Player(BasePlayer):
-    pass
+    endowment = models.CurrencyField()
+    cost_per_ticket = models.CurrencyField()
+    tickets_purchased = models.IntegerField()
+    prize_won = models.FloatField()
+    earnings = models.CurrencyField()
+
+
+    def setup_round(self):
+        self.endowment = C.ENDOWMENT
+        self.cost_per_ticket = C.COST_PER_TICKET
+
+
+    @property
+    def coplayer(self):
+        return self.group.get_player_by_id(3 - self.id_in_group)
+
 
 
 # PAGES
 
-class setupround(WaitPage):
-    pass
+class SetupRound(WaitPage):
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        subsession.setup_round()
+
+
 
 
 class Intro(Page):
@@ -35,15 +74,21 @@ class Intro(Page):
 
 
 class Decision(WaitPage):
-    pass
+    form_model= "player"
+    form_fields = ["tickets_purchased"]
 
 
-class decisionwaitpage(WaitPage):
+
+class DecisionWaitPage(WaitPage):
     pass
 
 
 class Results(WaitPage):
-    pass
+    @staticmethod
+    def vars_for_template(player):
+        return {
+            "tickets_purchased": player.tickets_purchased,
+        }
 
 
 class Endblock(WaitPage):
@@ -52,4 +97,11 @@ class Endblock(WaitPage):
 
 
 
-page_sequence = [Intro, Decision, Results,Endblock,]
+page_sequence = [
+    SetupRound,
+    Intro,
+    Decision,
+    DecisionWaitPage,
+    Results,
+    Endblock,
+]
